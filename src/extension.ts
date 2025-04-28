d1ec89ed86669f2b7725e8c94cf54e229cd98a42
ChrisKim <1515748548@qq.com>
2025-04-28 16:57:29 +0800

    [feat] hello world

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,57 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "llm-chat" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('llm-chat.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from llm-chat!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      "chatView",
+      new ChatViewProvider(context)
+    )
+  );
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export class ChatViewProvider implements vscode.WebviewViewProvider {
+  constructor(private context: vscode.ExtensionContext) { }
+
+  resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    webviewView.webview.options = {
+      enableScripts: true,
+    };
+
+    webviewView.webview.html = this.getWebviewContent();
+
+    webviewView.webview.onDidReceiveMessage(async (message) => {
+      switch (message.command) {
+        case "showMessage":
+          vscode.window.showInformationMessage(message.text);
+          break;
+      }
+    });
+  }
+
+  getWebviewContent() {
+    return `
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>llm-chat</h1>
+    <button onclick="sendMessage()">hello world!</button>
+    <script>
+        const vscode = acquireVsCodeApi();
+        function sendMessage() {
+            vscode.postMessage({
+                command: 'showMessage',
+                text: 'Hello!'
+            });
+        }
+    </script>
+</body>
+</html>
+`;
+  }
+}
+
+export function deactivate() { }
